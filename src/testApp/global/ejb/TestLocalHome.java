8b316ecbe7be37b13ac955743fc31a2fcb363c9e
sanjay <sanjay@global.com>
2011-06-26 21:42:57 +0530

    Finally completed session/entity beans together on 16/06/2011.

--- src/testApp/global/ejb/TestLocalHome.java
+++
@@ -1,9 +0,0 @@
-package global.ejb;
-
-import javax.ejb.*;
-import java.rmi.*;
-
-public interface TestLocalHome extends EJBLocalHome
-{
-   public TestLocalComponent create() throws CreateException;
-}
